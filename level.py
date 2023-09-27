371d4aab8f74e5f13c0e2c605c0a411be8ef8f63
Hallis1221 <halvorviv@gmail.com>
2023-09-27 13:05:54 +0200

    :+1: Add a frame and choices to rooms

--- level.py
+++ level.py
@@ -1,4 +1,5 @@
 from pick import pick
+from time import sleep
 
 class Level:
     def __init__(self, rooms, player):
@@ -23,20 +24,46 @@ class Level:
                 if x == self.player.x and y == self.player.y:
                     room_fill.append("x")
                 else:
-                    room_fill.append("o")
+                    room_fill.append("y")
 
         i = 1
         for y in range(0,self.current_room.y):
+            if (y == 0):
+                print(" ",end="")
+                for x in range(0,self.current_room.x+2):
+                    print("-", end="")
+                print("")
             for x in range(0,self.current_room.x):
+                if x == 0:
+                    print("| ", end="")
                 print(room_fill[i], end="")
+                if x == self.current_room.x-1:
+                    print(" |", end="")
                 i += 1
-            print("")
-        
-        
                 
-        print(
-        """      
-        """)
+          
+            print("")
+            tekst = ""
+            if y ==self.current_room.y-1:
+                tekst += " "
+                for x in range(0,self.current_room.x+2):
+                    tekst += "-"
+                tekst += "\n"
+            print(tekst, end="")
+        input("Press Enter for å se valg. ")
+
+    
+
+    
+    def draw_room_with_choices(self, choices):
+        self.draw_room()
+        option, index = pick(choices, "Velg hva du vil gjøre", indicator='=>', default_index=0, )
+
+
+
+
+         
+        
 
 class Room:
     def __init__(self, x,y):
@@ -55,4 +82,4 @@ r2 = Room(x=5,y=5)
 p = Player(x=1,y=1)
 level = Level([r1,r2],p)
 level.pick_room()
-level.draw_room()
+level.draw_room_with_choices(["1","2"])
