1a59c06bbe02e56c25c02c95c8b91b725d3a33c7
Lars-M-E <lmegranlund@gmail.com>
2023-09-27 11:29:38 +0200

    La til posisjon og en liten backstory

--- Enemy/Warrior.py
+++ Enemy/Warrior.py
@@ -1,5 +1,5 @@
 import index as i
 
 class Warrior(i.Enemey):
-    def __init__(self, hp = 40, name = "Warrior", attack = 5, speed = 4, reach = 4):
-       super().__init__(hp, name, attack, speed, reach) 
+    def __init__(self, hp = 40, name = "Warrior", attack = 5, speed = 4, reach = 4, xpos = 50, ypos = 0):
+       super().__init__(hp, name, attack, speed, reach, xpos, ypos) 
--- Enemy/index.py
+++ Enemy/index.py
@@ -1,11 +1,12 @@
 class Enemy:
-    def __init__(self, hp = 40, name = "Goon", attack = 3, speed = 2, reach = 1):
+    def __init__(self, hp = 40, name = "Goon", attack = 3, speed = 2, reach = 1, xpos = 50, ypos = 0):
         self.hp = hp
         self.name = name
         self.attack = attack
         self.speed = speed
         self.reach = reach
-
+        self.xpos = xpos
+        self.ypos = ypos
 
 
 
