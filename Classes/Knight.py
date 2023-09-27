bceb881a8f60d2eb092e6c68ad76d801810a8a79
Hallis1221 <halvorviv@gmail.com>
2023-09-27 17:39:27 +0200

    Fix imports

--- Classes/Knight.py
+++ Classes/Knight.py
@@ -1,5 +1,5 @@
-from Enemy.index import Enemydx
+from Classes.index import Player
 
-class Knight(idx.Player):
+class Knight(Player):
     def __init__(self, hp=70, attack=4, name="Knight",reach=2,speed=2):
       super.__init__(hp, attack,name,reach,speed)
--- Classes/Sharpshooter.py
+++ Classes/Sharpshooter.py
@@ -1,5 +1,5 @@
-from Enemy.index import Enemydx
+from Classes.index import Player
 
-class Sharpshooter(idx.Player):
+class Sharpshooter(Player):
     def __init__(self, name="Sharpshooter", hp=20, attack=5, reach=10,speed=3):
       super.__init__(hp, attack,name,reach,speed)
