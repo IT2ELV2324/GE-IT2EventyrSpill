ee318e52192b55665456a3063992d8ccefe08634
Hallis1221 <halvorviv@gmail.com>
2023-10-14 11:17:12 +0200

    Update index.py

--- Classes/index.py
+++ Classes/index.py
@@ -1,7 +1,6 @@
 import math
 from os import system
 from Classes.index import Player
-from Enemy.index import Enemy
 class Player:
 
    def __init__(self, hp=0, attack=0, name="",reach=0,speed=0,xpos=10,ypos=0, apperance="👨"):
@@ -15,7 +14,7 @@ class Player:
       self.apperance = apperance
    
    def check_if_within_reach(self, xpos,ypos):
-      if (abs(self.xpos - Enemy.xpos) <= self.reach) and abs(self.ypos - Enemy.ypos) <= self.reach:
+      if (abs(self.xpos - xpos) <= self.reach) and abs(self.ypos - ypos) <= self.reach:
          return True
       else:
          return False
